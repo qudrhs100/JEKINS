58626220e5ad791c1338e265880ec243f05271e1
김병곤 <qudrhs190@gmail.com>
2019-03-11 11:37:05 +0900

    0311

--- src/test.java
+++ src/test.java
@@ -1,8 +1,6 @@
 public class test{
-	
 	public static void main(String[] args) {
-		
 		System.out.println("Hello world");
-		System.out.println("test");
+		System.out.println("Hello world");
 	}
 }
